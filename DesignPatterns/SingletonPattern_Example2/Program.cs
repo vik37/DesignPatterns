7e875f2f92785a0ba010fca700673a6871f04749
vik37 <59177404+vik37@users.noreply.github.com>
2022-01-03 20:26:18 +0100

    Design Pattern Singleton 2nd Example

---
+++ DesignPatterns/SingletonPattern_Example2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SingletonPattern_Example2
+{
+    class Program
+    {
+        static TableServers host1List = TableServers.GetTableServers();
+        static TableServers host2List = TableServers.GetTableServers();
+        static void Main(string[] args)
+        {
+            TableServers servers = TableServers.GetTableServers();
+            for (int i = 0; i < 5; i++)
+            {
+                //Console.WriteLine("The next server is: " + servers.GetNextServer());
+                HostOneGetNextServer();
+                HostTwoGetNextServer();
+            }
+            Console.ReadLine();
+        }
+        public static void HostOneGetNextServer()
+        {
+            Console.WriteLine("The next server is: " + host1List.GetNextServer());
+        }
+        public static void HostTwoGetNextServer()
+        {
+            Console.WriteLine("The next server is: " + host2List.GetNextServer());
+        }
+    }
+}
---
+++ DesignPatterns/SingletonPattern_Example2/TableServers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern_Example2
+{
+    public class TableServers
+    {
+        private static readonly TableServers _instance = new TableServers();
+        private List<string> _servers = new List<string>();
+        private int _nextServer = 0;
+        private TableServers()
+        {
+            _servers.Add("Viktor");
+            _servers.Add("Sue");
+            _servers.Add("Mary");
+            _servers.Add("Marinko");
+        }
+        public static TableServers GetTableServers()
+        {
+            return _instance;
+        }
+        public string GetNextServer()
+        {
+            string output = _servers[_nextServer];
+            _nextServer += 1;
+            if(_nextServer >= _servers.Count)
+            {
+                _nextServer = 0;
+            }
+            return output;
+        }
+    }
+}
