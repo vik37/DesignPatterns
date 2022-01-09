2ab48453dbf29d7b31138b908f0a72dce408077e
vik37 <59177404+vik37@users.noreply.github.com>
2022-01-09 21:30:34 +0100

    Open Close Principle (OCP)
    There are 2 apps. First is initial app example and second is change with Open Close Principle. Refactor to new design code.

---
+++ DesignPatterns/OpenClosePrinciple/InitialCode/OpenSolid_Principle/Program.cs
@@ -0,0 +1,34 @@
+﻿using OCPLibrary;
+using OCPLibrary.Accounts;
+using OCPLibrary.Applicants;
+using System;
+using System.Collections.Generic;
+
+namespace OpenSolid_Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> applicants = new List<PersonModel>()
+            {
+                new PersonModel(){Firstname="Viktor", Lastname="Zafirovski"},
+                new PersonModel(){Firstname="Sarma", Lastname="Posna"},
+                new PersonModel(){Firstname="Slagjana", Lastname="Trajkovska"}
+            };
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Account accountProccessor = new Account();
+            foreach (var person in applicants)
+            {
+                employees.Add(accountProccessor.Create(person));
+            }
+            Console.WriteLine(" E M P L O Y E E S: ");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($" -------- \n First name: {employee.Firstname} \n\n Last name: {employee.Lastname} \n\n " +
+                    $"E-mail: {employee.Email} \n\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPatterns/OpenClosePrinciple/RefactorToOCPCode/OpenSolid_Principle/Program.cs
@@ -0,0 +1,34 @@
+﻿using OCPLibrary;
+using OCPLibrary.Accounts;
+using OCPLibrary.Applicants;
+using System;
+using System.Collections.Generic;
+
+namespace OpenSolid_Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IApplicantModel> applicants = new List<IApplicantModel>()
+            {
+                new PersonModel(){Firstname="Viktor", Lastname="Zafirovski"},
+                new ManagerModel(){Firstname="Sarma", Lastname="Posna"},
+                new ExecutiveModel(){Firstname="Slagjana", Lastname="Trajkovska"}
+            };
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Account accountProccessor = new Account();
+            foreach (var person in applicants)
+            {
+                employees.Add(person.AccountProccessor.Create(person));
+            }
+            Console.WriteLine(" E M P L O Y E E S: ");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($" -------- \n First name: {employee.Firstname} \n\n Last name: {employee.Lastname} \n\n " +
+                    $"E-mail: {employee.Email} \n\n is manager: {employee.isManager} \n\n is executive: {employee.isExecutive}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
