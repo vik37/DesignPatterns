409ac312e32619b1f36345995ca4b88f96c45f9b
vik37 <59177404+vik37@users.noreply.github.com>
2022-01-20 20:30:52 +0100

    Dependency Inversion Principle
    It's simple app before refactor code with DI Principle.

---
+++ DesignPatterns/DependencyInversionPrinciple/SimpleCode/DemoLibrary/Emailer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLibrary
+{
+    public class Emailer
+    {
+        public void SendEmail(Person person, string message)
+        {
+            Console.WriteLine($"Simulating sending an email to { person.EmailAddress }");
+        }  
+    }
+}
---
+++ DesignPatterns/DependencyInversionPrinciple/SimpleCode/Dependensy_Inversion_Principle/Program.cs
@@ -0,0 +1,28 @@
+﻿using DemoLibrary;
+using System;
+
+namespace Dependensy_Inversion_Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person owner = new Person
+            {
+                FirstName = "Viktor",
+                LastName = "Zafirovski",
+                EmailAddress = "zaf.vik@viktor.org",
+                PhoneNumber = "555-1116"
+            };
+            Chore chore = new Chore
+            {
+                ChoreName = "Take out the trash",
+                Owner = owner
+            };
+            chore.PerformedWork(3);
+            chore.PerformedWork(1.5);
+            chore.ComplateChore();
+            Console.ReadLine();
+        }
+    }
+}
